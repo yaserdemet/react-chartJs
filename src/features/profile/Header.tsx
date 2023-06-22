9715fa78f0b1b476c83eaaa2f21c84314a926d9d
yaserdemet <ddemety372@gmail.com>
2023-06-23 00:11:29 +0300

    tabs

--- src/features/profile/Header.tsx
+++ src/features/profile/Header.tsx
@@ -2,40 +2,70 @@ import React from 'react';
 import { Stack, Card, Avatar, Typography } from '@mui/material';
 import { blue, blueGrey } from '@mui/material/colors';
 import { CustomAvatar } from 'src/components/custom-avatar';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import PhoneIcon from '@mui/icons-material/Phone';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import PersonPinIcon from '@mui/icons-material/PersonPin';
+import PhoneMissedIcon from '@mui/icons-material/PhoneMissed';
 import { useAuthContext } from 'src/auth/useAuthContext';
 import User from '../../assets/illustrations/overlay_2.jpg';
 
 function Header() {
   const { user, logout } = useAuthContext();
+  const [value, setValue] = React.useState(0);
 
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
   return (
-    <Card
-      sx={{
-        backgroundImage: `url(${User})`, // Add "url()" around the image URL
-        py: 12,
-        px: 2,
-        overflow: 'visible',
-      }}
-    >
-      <CustomAvatar
-        sx={{ width: 128, height: 128, position: 'absolute', bottom: -24 }}
-        src={user?.photoURL}
-        alt={user?.displayName}
-        name={user?.displayName}
-      />
-      <Typography
-        variant="h4"
-        sx={{ position: 'absolute', bottom: 36, left: 160, color: blueGrey[900] }}
+    <>
+      <Card
+        sx={{
+          backgroundImage: `url(${User})`, // Add "url()" around the image URL
+          py: 12,
+          px: 2,
+          overflow: 'visible',
+        }}
       >
-        {user?.displayName}
-      </Typography>
+        <CustomAvatar
+          sx={{ width: 128, height: 128, position: 'absolute', bottom: -24 }}
+          src={user?.photoURL}
+          alt={user?.displayName}
+          name={user?.displayName}
+        />
+        <Typography
+          variant="h4"
+          sx={{ position: 'absolute', bottom: 36, left: 160, color: blueGrey[900] }}
+        >
+          {user?.displayName}
+        </Typography>
+        <Typography
+          sx={{ position: 'absolute', bottom: 4, opacity: 0.72, left: 160, color: blueGrey[900] }}
+        >
+          {user?.role}
+        </Typography>
+      </Card>
+      <Tabs
+        sx={{
+          width: 1,
 
-      <Typography
-        sx={{ position: 'absolute', bottom: 4, opacity: 0.72, left: 160, color: blueGrey[900] }}
+          '& .MuiTabs-flexContainer': {
+            justifyContent: {
+              sm: 'center',
+              md: 'flex-end',
+            },
+          },
+        }}
+        value={value}
+        onChange={handleChange}
+        aria-label="icon position tabs example"
       >
-        {user?.role}
-      </Typography>
-    </Card>
+        <Tab icon={<PhoneMissedIcon />} iconPosition="start" label="start" />
+        <Tab icon={<PhoneMissedIcon />} iconPosition="start" label="start" />
+        <Tab icon={<PhoneMissedIcon />} iconPosition="start" label="start" />
+      </Tabs>
+    </>
   );
 }
 
