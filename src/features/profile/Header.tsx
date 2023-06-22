d7d41dd98a274d6df8f16b89b51c0634b0a8c052
yaserdemet <ddemety372@gmail.com>
2023-06-22 23:53:00 +0300

    BG

--- src/features/profile/Header.tsx
+++ src/features/profile/Header.tsx
@@ -1,22 +1,41 @@
 import React from 'react';
-import { Stack, Card, Box, Paper } from '@mui/material';
-import { blue } from '@mui/material/colors';
+import { Stack, Card, Avatar, Typography } from '@mui/material';
+import { blue, blueGrey } from '@mui/material/colors';
 import { CustomAvatar } from 'src/components/custom-avatar';
-import User from '../../assets/illustrations/overlay_3.jpg';
+import { useAuthContext } from 'src/auth/useAuthContext';
+import User from '../../assets/illustrations/overlay_2.jpg';
 
 function Header() {
+  const { user, logout } = useAuthContext();
+
   return (
-    <Paper
+    <Card
       sx={{
-        width: '100%',
-        height: '20vh',
-        backgroundImage: User,
-        p: 4,
-        
+        backgroundImage: `url(${User})`, // Add "url()" around the image URL
+        py: 12,
+        px: 2,
+        overflow: 'visible',
       }}
     >
-      <CustomAvatar src={User} name='Yaser'  />
-    </Paper>
+      <CustomAvatar
+        sx={{ width: 128, height: 128, position: 'absolute', bottom: -24 }}
+        src={user?.photoURL}
+        alt={user?.displayName}
+        name={user?.displayName}
+      />
+      <Typography
+        variant="h4"
+        sx={{ position: 'absolute', bottom: 36, left: 160, color: blueGrey[900] }}
+      >
+        {user?.displayName}
+      </Typography>
+
+      <Typography
+        sx={{ position: 'absolute', bottom: 4, opacity: 0.72, left: 160, color: blueGrey[900] }}
+      >
+        {user?.role}
+      </Typography>
+    </Card>
   );
 }
 
