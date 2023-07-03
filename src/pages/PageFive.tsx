84554cc764e702612c9487c89f9b68321675707b
yaserdemet <ddemety372@gmail.com>
2023-07-03 23:47:38 +0300

    settings

--- src/pages/PageFive.tsx
+++ src/pages/PageFive.tsx
@@ -1,6 +1,8 @@
 import { Helmet } from 'react-helmet-async';
 // @mui
 import { Container, Typography } from '@mui/material';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { useBattery } from 'react-use';
 // components
 import { useSettingsContext } from '../components/settings';
 
@@ -8,6 +10,8 @@ import { useSettingsContext } from '../components/settings';
 
 export default function PageFive() {
   const { themeStretch } = useSettingsContext();
+  const battery: any = useBattery();
+  const { isSupported, level, charging, dischargingTime, chargingTime } = battery;
 
   return (
     <>
@@ -21,21 +25,15 @@ export default function PageFive() {
         </Typography>
 
         <Typography gutterBottom>
-          Curabitur turpis. Vestibulum facilisis, purus nec pulvinar iaculis, ligula mi congue nunc,
-          vitae euismod ligula urna in dolor. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit
-          id, lorem. Phasellus blandit leo ut odio. Vestibulum ante ipsum primis in faucibus orci
-          luctus et ultrices posuere cubilia Curae; Fusce id purus. Aliquam lorem ante, dapibus in,
-          viverra quis, feugiat a, tellus. In consectetuer turpis ut velit. Aenean posuere, tortor
-          sed cursus feugiat, nunc augue blandit nunc, eu sollicitudin urna dolor sagittis lacus.
-          Vestibulum suscipit nulla quis orci. Nam commodo suscipit quam. Sed a libero.
-        </Typography>
-
-        <Typography>
-          Praesent ac sem eget est egestas volutpat. Phasellus viverra nulla ut metus varius
-          laoreet. Curabitur ullamcorper ultricies nisi. Ut non enim eleifend felis pretium feugiat.
-          Donec mi odio, faucibus at, scelerisque quis, convallis in, nisi. Fusce vel dui. Quisque
-          libero metus, condimentum nec, tempor a, commodo mollis, magna. In enim justo, rhoncus ut,
-          imperdiet a, venenatis vitae, justo. Cras dapibus.
+          {/* Display Battery Charge Percentage */}
+          <strong>Charge level</strong>:&nbsp;&nbsp; <span>{(level * 100).toFixed(0)}%</span> <br />
+          {/* Display Whether Device is charging or not */}
+          <strong>Charging</strong>:&nbsp;&nbsp; <span>{charging ? 'Yes' : 'No'}</span> <br />
+          {/* Display Device Charging time */}
+          <strong>Charging time</strong>:&nbsp;&nbsp;
+          <span>{chargingTime && chargingTime }</span> <br />
+          {/* Display Device Discharging time */}
+          <strong>Discharging time</strong>:&nbsp;&nbsp; <span>{dischargingTime}</span>
         </Typography>
       </Container>
     </>
