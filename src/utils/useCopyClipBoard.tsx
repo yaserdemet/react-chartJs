573471844b26b3fd68eb585e2ddf9a6023848ad6
yaserdemet <ddemety372@gmail.com>
2023-07-27 23:28:37 +0300

    copyClipboard done!

--- src/utils/useCopyClipBoard.tsx
+++ src/utils/useCopyClipBoard.tsx
@@ -1,14 +1,27 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useState } from 'react';
 
-const useCopyClipBoard = ({text} : {text : string}) => {
-    const [copied, setCopied] = useState(false);
-         
-        const copyText = useCallback( async () => {
-                    if(!navigator.clipboard){
-                        console.warn("Your Browser Cant Support Clipboard!")
-                    }
-        },[text])
+const useCopyClipBoard = ({ text }: { text: string }) => {
+  const [copied, setCopied] = useState(false);
 
-}
+  const copyText = useCallback(async () => {
+    if (!navigator.clipboard) {
+      console.warn('Your Browser Cant Support Clipboard!');
+      setCopied(false);
+      return;
+    }
+    try {
+      const response = await navigator.clipboard.writeText(text);
+      setCopied(true);
+      setTimeout(() => {
+        setCopied(false);
+      }, 2024);
+    } catch (e) {
+      console.warn(e);
+      setCopied(false);
+    }
+  }, [text]);
 
-export default useCopyClipBoard
+  return [copied, copyText];
+};
+
+export default useCopyClipBoard;
