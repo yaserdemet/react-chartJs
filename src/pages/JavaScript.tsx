27da0e065db7ce330935df9eb979988e1dceb387
yaserdemet <ddemety372@gmail.com>
2023-07-27 23:45:50 +0300

    test-clipboard

--- src/pages/JavaScript.tsx
+++ src/pages/JavaScript.tsx
@@ -1,16 +1,18 @@
 import { Helmet } from 'react-helmet-async';
 // @mui
-import { Container, Typography } from '@mui/material';
+import { Button, Container, Typography } from '@mui/material';
 // components
 import Header from 'src/features/js/Header';
 import UsefullJs from 'src/features/js/UsefullJs';
 import { useSettingsContext } from '../components/settings';
+import useCopyClipBoard from 'src/utils/useCopyClipBoard';
 
 // ----------------------------------------------------------------------
 
 export default function PageTwo() {
   const { themeStretch } = useSettingsContext();
 
+  const [copied, copyText] = useCopyClipBoard('hellöö');
   return (
     <>
       <Helmet>
@@ -21,6 +23,7 @@ export default function PageTwo() {
         <Typography variant="h3" component="h1" paragraph>
           JavaScript
         </Typography>
+        <Button onClick={() => copyText()}>copy</Button>
         <Header />
         <UsefullJs />
       </Container>
--- src/utils/useCopyClipBoard.tsx
+++ src/utils/useCopyClipBoard.tsx
@@ -1,23 +1,26 @@
 import React, { useCallback, useState } from 'react';
 
-const useCopyClipBoard = ({ text }: { text: string }) => {
+const useCopyClipBoard = (text : string) : [boolean, () => Promise<void>] => {
   const [copied, setCopied] = useState(false);
 
   const copyText = useCallback(async () => {
     if (!navigator.clipboard) {
       console.warn('Your Browser Cant Support Clipboard!');
       setCopied(false);
-      return;
+      
     }
-    try {
-      const response = await navigator.clipboard.writeText(text);
-      setCopied(true);
-      setTimeout(() => {
-        setCopied(false);
-      }, 2024);
-    } catch (e) {
-      console.warn(e);
-      setCopied(false);
+    else {
+
+        try {
+          const response = await navigator.clipboard.writeText(text);
+          setCopied(true);
+          setTimeout(() => {
+            setCopied(false);
+          }, 2024);
+        } catch (e) {
+          console.warn(e);
+          setCopied(false);
+        }
     }
   }, [text]);
 
