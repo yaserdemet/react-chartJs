fbeb2077bfb0fdbcb43c492f4a7ebcd55874e7cd
yaserdemet <ddemety372@gmail.com>
2023-08-02 09:08:10 +0300

    new variables

--- src/pages/JavaScript.tsx
+++ src/pages/JavaScript.tsx
@@ -5,33 +5,22 @@ import { Button, Container, Typography } from '@mui/material';
 import Header from 'src/features/js/Header';
 import UsefullJs from 'src/features/js/UsefullJs';
 import { useSettingsContext } from '../components/settings';
-import useCopyClipBoard from 'src/utils/useCopyClipBoard';
-import { useStore } from 'src/zustand/store';
+
 
 // ----------------------------------------------------------------------
 
 export default function PageTwo() {
   const { themeStretch } = useSettingsContext();
 
-  const [copied, copyText] = useCopyClipBoard('hellöö');
-  const {inputValue, changeInputValue, count, countUp} = useStore();
-  console.log(inputValue);
-  const handleChange = (event) => {
-    changeInputValue(event.target.value);
-  }
   return (
     <>
       <Helmet>
         <title> JavaScript | Usefull Notes</title>
       </Helmet>
-    <input  value={inputValue} onChange={handleChange}/>
-    {inputValue}
-    {count}
       <Container maxWidth={themeStretch ? false : 'xl'}>
         <Typography variant="h3" component="h1" paragraph>
           JavaScript
         </Typography>
-        <Button onClick={countUp}>copy</Button>
         <Header />
         <UsefullJs />
       </Container>
--- src/zustand/store.tsx
+++ src/zustand/store.tsx
@@ -3,8 +3,13 @@ import { _notifications } from 'src/_mock/arrays';
 interface Store {
   totalUnRead: number;
   onClickBadge: VoidFunction;
+  initialInputValue : string;
 }
 export const useStore = create<Store>((set) => ({
   totalUnRead: _notifications.filter((item: any) => item.isUnRead === true).length,
   onClickBadge: () => set({ totalUnRead: 0 }),
+  initialInputValue : "",
+  changeInputValue : (value : any) => set({initialInputValue: value }),
+  count : 0,
+  countUp : 
 }));
