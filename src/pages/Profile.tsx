6938baf2cb462af561467b817eeddbc994bf9bec
yaserdemet <ddemety372@gmail.com>
2023-08-07 00:15:07 +0300

    framer-type

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -66,10 +66,10 @@ export default function PageFour() {
             {currentTab === 'Friends' && (
               <Box
                 component={m.div}
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                exit={{ opacity: 0 }}
-                transition={{ duration: 0.5 }}
+                initial={{ opacity: 0, y : 300 }}
+                animate={{ opacity: 1, y : 0 }}
+                exit={{ opacity: 0, }}
+                transition={{ type : "spring",}}
               >
                 <Friends />
               </Box>
